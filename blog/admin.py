baf9c496338ac5f266d9c35eef128afdb2f77afc
amirreza <amirezapassandi@gmail.com>
2021-10-06 15:09:18 -0700

    add Blog model to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Blog
+
 # Register your models here.
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title','slug','author','is_special','status')
+    search_fields = ('title','author','category__title')
+    list_filter = ('status','is_special','publish')
+    list_per_page = 30
