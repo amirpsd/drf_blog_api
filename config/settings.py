5a196b9a2adedf9293d65a5d4a1bee19365cdf77
amirreza <amirezapassandi@gmail.com>
2021-10-07 14:40:06 -0700

    add User model in account app

--- config/settings.py
+++ config/settings.py
@@ -132,3 +132,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'account.User'
