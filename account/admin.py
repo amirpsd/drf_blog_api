9e4d2c5bfd9f3e9747211bfeac437b6936fa37b8
amirreza <amirezapassandi@gmail.com>
2021-10-09 16:59:55 -0700

    add one field in User model in account app

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
 # Register your models here.
+
+UserAdmin.fieldsets[2][1]['fields'] = (
+    'is_active',
+    'is_staff',
+    'is_superuser',
+    'groups',
+    'user_permissions',
+    'author',
+    )
+UserAdmin.list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'author')
+
+
+
+admin.site.register(User, UserAdmin)
--- account/models.py
+++ account/models.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 # Create your models here.
 
 class User(AbstractUser):
-    pass
+    author = models.BooleanField(default=False, blank=True)
