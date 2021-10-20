136c96629f6b2449f8bd954be44160e5dbde9c23
amirreza <amirezapassandi@gmail.com>
2021-10-20 12:05:42 -0700

    Add another permissions

--- permissions.py
+++ permissions.py
@@ -1,5 +1,15 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
+
+class IsSuperUser(BasePermission):
+    message = 'You Must Be SuperUser'
+
+    def has_permission(self, request, view):
+        return bool(
+            request.user.is_authenticated and request.user.is_superuser
+        )
+
+
 class IsSuperUserOrReadOnly(BasePermission):
     
     def has_permission(self, request, view):
