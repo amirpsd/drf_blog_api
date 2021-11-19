1427fc7d85cb1f56c1d7fe3440b03eaa8b81c107
amirreza <amirezapassandi@gmail.com>
2021-11-19 10:16:20 -0800

    Add verbose_name and Translator for verbose_name in blog.models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from django.utils.translation import gettext as _
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 from django.db import models
@@ -16,8 +17,8 @@ User = get_user_model()
 
 class Blog(models.Model):
     STATUS_CHOICES = (
-        ('p','publish'),
-        ('d','draft'),
+        ("p", "publish"),
+        ("d", "draft"),
     )
     author = models.ForeignKey(
         User,
@@ -25,45 +26,53 @@ class Blog(models.Model):
         null=False,
         blank=False,
         on_delete=models.CASCADE,
-        related_name='blogs',
+        related_name="blogs",
+        verbose_name=_("Author"),
     )
     category = models.ManyToManyField(
         Category,
         default=None,
         blank=True,
         related_name="blogs",
+        verbose_name=_("Categories"),
     )
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True,blank=True)
-    body = models.TextField(blank=False)
-    image = models.ImageField(upload_to=upload_file_path)
-    summary = models.TextField(max_length=400)
+    title = models.CharField(max_length=200, verbose_name=_("Title"))
+    slug = models.SlugField(
+        unique=True,
+        blank=True,
+        verbose_name=_("Slug"),
+        help_text=_("Do not fill in here"),
+    )
+    body = models.TextField(blank=False, verbose_name=_("Content"))
+    image = models.ImageField(upload_to=upload_file_path, verbose_name=_("Image"))
+    summary = models.TextField(max_length=400, verbose_name=_("Summary"))
     likes = models.ManyToManyField(
         get_user_model(),
         blank=True,
-        related_name='likes'
+        related_name="likes",
+        verbose_name=_("Likes"),
     )
     dislikes = models.ManyToManyField(
         get_user_model(),
         blank=True,
-        related_name='dislikes'
+        related_name="dislikes",
+        verbose_name=_("Dislikes"),
     )
-    publish = models.DateTimeField(default=timezone.now)
-    create = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    special = models.BooleanField(default=False)
-    status = models.CharField(max_length=1,choices=STATUS_CHOICES)
-    visits = models.PositiveIntegerField(default=0)
-
+    publish = models.DateTimeField(default=timezone.now, verbose_name=_("Publish time"))
+    create = models.DateTimeField(auto_now_add=True, verbose_name=_("Create time"))
+    updated = models.DateTimeField(auto_now=True, verbose_name=_("Update time"))
+    special = models.BooleanField(default=False, verbose_name=_("Is special Blog ?"))
+    status = models.CharField(
+        max_length=1, choices=STATUS_CHOICES, verbose_name=_("Status")
+    )
+    visits = models.PositiveIntegerField(default=0, verbose_name=_("Visits"))
 
     def __str__(self):
-        return  self.author.username + " | " + self.title 
+        return self.author.username + " | " + self.title
 
-    
     class Meta:
-        ordering = ['-publish','-updated']
-        verbose_name = "blog"
-        verbose_name_plural = "blogs"
-
+        ordering = ["-publish", "-updated"]
+        verbose_name = _("Blog")
+        verbose_name_plural = _("Blogs")
 
     objects = BlogManager()
