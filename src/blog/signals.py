016104f64ab44e547b30dc840ca84fad6e1a295e
amirreza <amirezapassandi@gmail.com>
2021-12-18 16:34:14 -0800

    add delete_media_blog signals

--- src/blog/signals.py
+++ src/blog/signals.py
@@ -1,12 +1,20 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_delete
 from django.utils.text import slugify
 from django.dispatch import receiver
 
 from extensions.code_generator import code_generator
 
+from os import getcwd, remove
 from .models import Blog
 
 @receiver(pre_save, sender=Blog)
-def save_slug_blog(sender,instance,*args,**kwargs):
+def save_slug_blog(sender, instance, *args, **kwargs):
     if len(instance.slug) <= 5:
-        instance.slug = slugify(code_generator(10)) 
+        instance.slug = slugify(code_generator(10))
+
+
+@receiver(post_delete, sender=Blog)
+def delete_media_blog(sender, instance, *args, **kwargs):
+    path = getcwd()
+    final_path = path + instance.image.url 
+    remove(final_path)
