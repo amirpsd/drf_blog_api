9bda82aedc2051d75d6514fbdf7967ac24da5ba5
amirreza <amirezapassandi@gmail.com>
2022-02-03 10:50:23 -0800

    Change the otp_generator function in the password generation method

--- src/extensions/code_generator.py
+++ src/extensions/code_generator.py
@@ -1,10 +1,11 @@
 from string import digits, ascii_lowercase, ascii_uppercase
-from random import choice
+from random import choice as rand_choice
+from secrets import choice as sec_choice
 
 
 def slug_generator(size: int = 10, char: str = digits + ascii_uppercase + ascii_lowercase) -> str:
-    return "".join(choice(char) for _ in range(size))
+    return "".join(rand_choice(char) for _ in range(size))
 
 
 def otp_generator(size: int = 6, char: str = digits) -> str:
-    return "".join(choice(char) for _ in range(size))
+    return "".join(sec_choice(char) for _ in range(size))
