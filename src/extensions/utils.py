cd2927985840e80c8b594b2c7d61499d314bd859
amirreza <amirezapassandi@gmail.com>
2022-06-23 12:43:04 -0700

    Rename imports

--- src/extensions/utils.py
+++ src/extensions/utils.py
@@ -1,6 +1,6 @@
 from string import digits, ascii_lowercase, ascii_uppercase
-from random import choice as rand_choice
-from secrets import choice as sec_choice
+from secrets import choice as secret_choice
+from random import choice as random_choice
 from os.path import basename, splitext
 
 
@@ -26,8 +26,8 @@ def get_client_ip(request) -> str:
 
 
 def slug_generator(size: int = 10, char: str = digits + ascii_uppercase + ascii_lowercase) -> str:
-    return "".join(rand_choice(char) for _ in range(size))
+    return "".join(random_choice(char) for _ in range(size))
 
 
 def otp_generator(size: int = 6, char: str = digits) -> str:
-    return "".join(sec_choice(char) for _ in range(size))
+    return "".join(secret_choice(char) for _ in range(size))
